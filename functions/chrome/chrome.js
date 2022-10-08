f98f2fecee45b4998156b19136fb80014e7ccb5f
Jason Lawton <jason@jasonlawton.com>
2022-10-08 15:27:40 -0400

    Update chrome.js

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -5,50 +5,50 @@ exports.handler = async (event, context) => {
   let theTitle = null
   let browser = null
   console.log('spawning chrome headless')
-  try {
-    const executablePath = await chromium.executablePath
+//   try {
+//     const executablePath = await chromium.executablePath
 
-    // setup
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: executablePath,
-      headless: chromium.headless,
-    })
+//     // setup
+//     browser = await puppeteer.launch({
+//       args: chromium.args,
+//       executablePath: executablePath,
+//       headless: chromium.headless,
+//     })
 
-    // Do stuff with headless chrome
-    const page = await browser.newPage()
-    const targetUrl = 'https://davidwells.io'
+//     // Do stuff with headless chrome
+//     const page = await browser.newPage()
+//     const targetUrl = 'https://davidwells.io'
 
-    // Goto page and then do stuff
-    await page.goto(targetUrl, {
-      waitUntil: ["domcontentloaded", "networkidle0"]
-    })
+//     // Goto page and then do stuff
+//     await page.goto(targetUrl, {
+//       waitUntil: ["domcontentloaded", "networkidle0"]
+//     })
 
-    await page.waitForSelector('#phenomic')
+//     await page.waitForSelector('#phenomic')
 
-    theTitle = await page.title();
+//     theTitle = await page.title();
 
-    console.log('done on page', theTitle)
+//     console.log('done on page', theTitle)
 
-  } catch (error) {
-    console.log('error', error)
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error
-      })
-    }
-  } finally {
-    // close browser
-    if (browser !== null) {
-      await browser.close()
-    }
-  }
+//   } catch (error) {
+//     console.log('error', error)
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({
+//         error: error
+//       })
+//     }
+//   } finally {
+//     // close browser
+//     if (browser !== null) {
+//       await browser.close()
+//     }
+//   }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      title: theTitle,
+      title: "hello world", //theTitle
     })
   }
 }
